fec9ac51dc4bbf2cc8de3bcfaa8a7b038a20640f
Cristina Rus Morales <cristinarm2005@gmail.com>
2026-02-06 10:13:25 +0100

    clase

--- tareaenclase1411/src/main/java/com/salesianostriana/edu/tareaenclase1411/controller/GlobalErrorController.java
+++ tareaenclase1411/src/main/java/com/salesianostriana/edu/tareaenclase1411/controller/GlobalErrorController.java
@@ -1,12 +1,19 @@
 package com.salesianostriana.edu.tareaenclase1411.controller;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.salesianostriana.edu.tareaenclase1411.error.MonumentoNotFoundException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
+import lombok.Builder;
+import org.springframework.http.*;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalErrorController extends ResponseEntityExceptionHandler {
@@ -23,5 +30,48 @@ public class GlobalErrorController extends ResponseEntityExceptionHandler {
         result.setTitle("Monumento no encontrado.");
         return result;
     }
+
+    //@Override
+    protected ResponseEntity<Object> handleÇMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request){
+        ProblemDetail result = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Error de validación");
+        List<ApiValidationSubError> subErrors;
+    }
+    @Builder
+    record ApiValidationSubError(
+            String object,
+            String message,
+            @JsonInclude(JsonInclude.Include.NON_NULL)
+            String field,
+            @JsonInclude(JsonInclude.Include.NON_NULL)
+            Object rejectedValue
+    ){
+
+        public ApiValidationSubError(String object, String message){
+            this(object, message, null, null);
+        }
+//Para leer los mensajes personalizados se tienen que poner los beans
+// que hay en el validationConfig
+        public static ApiValidationSubError from(ObjectError error){
+            ApiValidationSubError result = null;
+
+            if (error instanceof FieldError fieldError){
+                result = ApiValidationSubError.builder()
+                        .object(fieldError.getObjectName())
+                        .message(fieldError.getDefaultMessage())
+                        .field(fieldError.getField())
+                        .rejectedValue(fieldError.getRejectedValue())
+                        .build();
+            } else {
+                result = ApiValidationSubError.builder()
+                        .object(error.getObjectName())
+                        .message(error.getDefaultMessage())
+                        .build();
+            }
+
+        }
+
+    }
+
+
 
 }
